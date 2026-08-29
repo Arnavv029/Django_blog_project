c413fe585fb299ee08411cde7b62cd6373a47f39
Arnav Tiwari <tiwariarnav460@gmail.com>
2026-08-29 15:36:33 +0530

    search functionlity

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>/', Blogviews.blogs, name="blog")
+    path('search/', Blogviews.search, name='search'),
+    path('<slug:slug>/', Blogviews.blogs, name="blog"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
--- blogs/views.py
+++ blogs/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Blog, Category
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
+from django.db.models import Q 
 
 # Create your views here.
 
@@ -29,4 +30,17 @@ def blogs(request, slug):
     }
 
     return render(request, "blogs.html", constext)
+
+def search(request): 
+
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter( Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status=1)
+    print(blogs)
+
+    context = { 
+        "blogs":blogs,
+    }
+    
+    
+    return render(request, "search.html", context)
 
