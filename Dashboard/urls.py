268b92131bfa5f485a761a83459abd8b6e618161
Arnav Tiwari <tiwariarnav460@gmail.com>
2026-09-02 22:56:51 +0530

    dashboard categories section complete

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('categories/', views.categories, name='categories'),
+    path('add_categories/', views.add_categories, name='add_categories'),
+    path('edit_categories/<int:pk>/', views.edit_categories, name='edit_categories'),
+    path('delete_categories/<int:pk>/', views.delete_categories, name='delete_categories'),
+
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
+from .forms import CategoryForms
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -16,3 +18,39 @@ def dashboard(request):
     }
 
     return render(request, 'dashboard/dashboard.html/', context)
+
+def categories(request) : 
+    return render(request, 'dashboard/categories.html/' )
+
+def add_categories(request): 
+
+    if request.method == 'POST' :
+        form = CategoryForms(request.POST)
+        if form.is_valid() : 
+            form.save()
+            return redirect('categories')
+
+    form = CategoryForms() 
+    context = { 
+        "form":form
+    }
+
+    return render(request, 'dashboard/add_categories.html',context )
+
+def edit_categories(request, pk): 
+    categories = get_object_or_404(Category, pk=pk)
+    if request.method == "POST": 
+        form = CategoryForms(request.POST, instance=categories)
+        if form.is_valid(): 
+            form.save()
+            return redirect('categories')
+    form = CategoryForms(instance=categories)
+    context = { 
+        'form':form
+    }
+    return render(request, 'dashboard/edit_categories.html', context )
+
+def delete_categories(request, pk): 
+    categories = get_object_or_404(Category, pk=pk)
+    categories.delete()
+    return redirect('categories')
